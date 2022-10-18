2f98b687787b123ecba5df6837b4c0be29601a0f
pvdk31 <pvdk.7@outlook.com>
2022-10-18 18:30:20 +0200

    Revert "Revert "l""
    This reverts commit ebbaf818920d1e074312eba9fa2aec5f0045c292.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
                 </div>
             </div>
             <div class="col">
-                <img src="https://cohenwoodworking.com/wp-content/uploads/2016/09/image-placeholder-500x500.jpg" class="d-block vh-100" alt="...">
+                <img src="https://cohenwoodworking.com/wp-content/uploads/2016/09/image-placeholder-500x500.jpg" class="d-block vw-100" alt="...">
             </div>
         </div>
     </div>
@@ -41,11 +41,13 @@
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
+                    <a href="{{ route('project') }}">
                     <img src="{{ Vite::asset('resources/media/woods.jpg') }}" class=" d-block w-100 h-100" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                         <h5>First slide label</h5>
                         <p>Some representative placeholder content for the first slide.</p>
                     </div>
+                    </a>
                 </div>
                 @foreach($projects as $project)
                 <div class="carousel-item">
